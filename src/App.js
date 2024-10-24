b76c82aa086eaee69356cf79f1a5dfc33e14caee
JacksonSmith43 <jessica444@gmx.at>
2024-10-24 18:40:20 +0200

    Changed some styles.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
 import { useState } from "react";
 
 
-function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button> // Once a square has been clicked, this onSquareClick calls the onSquareClick from <Square value={squares[n]} onSquareClick={() => handleClick(n)} />. Which then calls function handleClick(i) {
-}
-
-function calculateWinner(squares) {
-
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-
-  }
-  return null;
-
-}
-
 export default function Game() {
 
   const [history, setHistory] = useState([Array(9).fill(null)]);
@@ -53,7 +24,7 @@ export default function Game() {
     let description;
 
     if (move > 0) {
-      description = "Go to move #" + move + ".";
+      description = "You are at move #" + move + ".";
 
     } else {
       description = "Go to game start.";
@@ -139,3 +110,34 @@ function Board({ xIsNext, squares, onPlay }) {
   );
 }
 
+
+
+
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button> // Once a square has been clicked, this onSquareClick calls the onSquareClick from <Square value={squares[n]} onSquareClick={() => handleClick(n)} />. Which then calls function handleClick(i) {
+}
+
+function calculateWinner(squares) {
+
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+
+  }
+  return null;
+
+}
